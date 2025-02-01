b15fa28af88261cdfaa5ce26a9b591e6e4bc890b
Ni3omi <girasimov1337@gmail.com>
2025-02-01 15:35:55 +0500

    fix: time tracking table: svgs
    https://crm.lead-bro.ru/tasks?taskId=6065
    Последние 2 комментария

--- src/pages/TimeTracking/components/Table/components/Stats/index.jsx
+++ src/pages/TimeTracking/components/Table/components/Stats/index.jsx
@@ -1,30 +1,31 @@
 import React from 'react';
-import StatsWidget from "../../../../../../shared/Widget";
-import Icon from "../../../../../../shared/Icon";
-import  './Stats.sass'
+import StatsWidget from '../../../../../../shared/Widget';
+import Icon from '../../../../../../shared/Icon';
+import './Stats.sass';
 
 const TimeSpendingStats = ({ totalTime, totalCost }) => {
-    const formatTotalTime = (minutes) => {
-        const hours = Math.floor(minutes / 60);
-        const remainingMinutes = minutes % 60;
-        return `${hours} ч${remainingMinutes ? `, ${remainingMinutes} мин` : ''}`;
-    };
+  const formatTotalTime = (minutes) => {
+    const hours = Math.floor(minutes / 60);
+    const remainingMinutes = minutes % 60;
+    return `${hours} ч${remainingMinutes ? `, ${remainingMinutes} мин` : ''}`;
+  };
 
-    return (
-        <div className={'timetracking_container'}>
-            <StatsWidget
-                title="Времени затрачено"
-                value={formatTotalTime(totalTime)}
-                showChart={false}
-                icon={<Icon name="clock" size={24} />}
-            />
-            <StatsWidget
-                title="Стоимость затраченного времени"
-                sum={totalCost}
-                showChart={false}
-                icon={<Icon name="dollar" size={24} />}
-            />
-        </div>
-    );
+  return (
+    <div className={'timetracking_container'}>
+      <StatsWidget
+        title="Времени затрачено"
+        value={formatTotalTime(totalTime)}
+        showChart={false}
+        icon={'/time.svg'}
+      />
+      <StatsWidget
+        title="Стоимость затраченного времени"
+        sum={totalCost}
+        showChart={false}
+        icon={'/coins.svg'}
+      />
+      />
+    </div>
+  );
 };
-export default TimeSpendingStats
+export default TimeSpendingStats;
